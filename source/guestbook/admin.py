b65d9b252a8b25569974c6ae8cdda5af30aee049
Dastan T <dastan.turatbekov@gmail.com>
2022-11-12 12:26:24 +0600

    Завершение 1-этапа: Добавил записи. сохранил данные в фикстуре

--- source/guestbook/admin.py
+++ source/guestbook/admin.py
@@ -3,7 +3,7 @@ from guestbook.models import NoteModel
 
 
 class NotesAdmin(admin.ModelAdmin):
-    list_display = ['id', 'author', 'email', 'created_at']
+    list_display = ['id', 'author', 'email', 'status', 'created_at']
     list_filter = ['status']
     search_fields = ['author']
     readonly_fields = ['created_at', 'updated_at']
