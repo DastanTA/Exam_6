970d59e81963609f91d5ce4dbd12f4dedf235781
Dastan T <dastan.turatbekov@gmail.com>
2022-11-12 11:52:27 +0600

    models are created, migrations are made and apllied

--- source/guestbook/models.py
+++ source/guestbook/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+STATUS_CHOICES = [('active', 'активно'), ('blocked', 'заблокировано')]
+
+
+class NoteModel(models.Model):
+    name = models.CharField(max_length=40, null=False, blank=False, verbose_name='имя автора')
+    email = models.EmailField(null=False, blank=False, verbose_name='почта автора')
+    note = models.TextField(max_length=3000, null=False, blank=False, verbose_name='запись')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='время создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='время редактирования')
+    status = models.CharField(max_length=30, null=False, blank=False, choices=STATUS_CHOICES, default=STATUS_CHOICES[0][0], verbose_name='статус')
